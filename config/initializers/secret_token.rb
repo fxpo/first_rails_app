266d54879ef030b20b8484721740ac39b2697157
fxpo <dxfxpo@gmail.com>
2014-01-09 16:23:30 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FxpoFirstRails::Application.config.secret_token = 'ec99a36a4b653da63f777c17387855f6ac24c7a0e8d17571422c1daf937c546e49491daecbbff0dd4818393b125eb0f78ba6326c8a0904ce87dc66c70451ab02'
